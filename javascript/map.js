819a5774ecda1beee5ad26c4578ce5a5d7294435
Svetlana Vargo <drumminde@MacBook-Pro-Svetlana.local>
2019-08-12 18:33:04 +0300

    refactoring

--- javascript/map.js
+++ javascript/map.js
@@ -1,5 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Vyb3BhIiwiYSI6ImNqejE3bW1yNzA3bDYzY25ybWJ4NHh4d3UifQ.MhxxfPV4YoH0RdH5c7lZxA';
-    var map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11'
 });
+
+function addMarkersForMap(user) {
+    const marker = new mapboxgl.Marker()
+        .setLngLat(user.geometry.coordinates)
+        .addTo(map);
+}
--- javascript/user.js
+++ javascript/user.js
@@ -1,70 +1,58 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:4000/db', false);
-xhr.send();
+function renderUser(user) {
+const userList = document.querySelector('.user-list');
 
-const usersJSONtext = xhr.response;
-const usersJSON = JSON.parse(usersJSONtext);
+const userCard = document.createElement('div');
+userCard.className = 'user-card';
 
-for (let i = 0; i < usersJSON.features.length - 979; i++) {
-    let userId;
-    userId = usersJSON.features[i];
-    
-    class User {
-        constructor(name, email, avatar, coordinates) {
-            this.name = userId.properties['userName'];
-            this.email = userId.properties['email'];
-            this.url = userId.properties['url'];
-            this.avatar = userId.properties['avatar'];
-            this.coordinates = userId.geometry['coordinates'];
+const userAvatar = document.createElement('div');
+userAvatar.className = 'user-avatar';
+
+const avatarImg = document.createElement('img');
+avatarImg.className = 'user-avatar-img';
+avatarImg.src = user.properties['avatar'];;
+
+const userDescriptionWrap = document.createElement('div');
+userDescriptionWrap.className = 'user-description-wrap';
+
+const name = document.createElement('p');
+name.className = 'user-name';
+
+const email = document.createElement('p');
+email.className = 'user-email';
+
+const url = document.createElement('a');
+url.className = 'user-url';
+url.href = user.properties['url'];
+url.target = '_blank';
+
+name.innerText = user.properties['userName'];
+email.innerText = user.properties['email'];;
+url.innerText = user.properties['url']
+userList.appendChild(userCard);
+userCard.appendChild(userAvatar);
+userAvatar.appendChild(avatarImg);
+userCard.appendChild(userDescriptionWrap);
+userDescriptionWrap.appendChild(name);
+userDescriptionWrap.appendChild(email);
+userDescriptionWrap.appendChild(url);
+};
+
+function parseDataFromServerAndRenderUsers(data) {
+    const users = JSON.parse(data).features;
+    users.forEach(user => {
+        renderUser(user);
+        addMarkersForMap(user);
+    })
+};
+
+function getDataFromServer() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:4000/db');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            parseDataFromServerAndRenderUsers(xhr.responseText);
         }
     }
-    
-    const userCardList = [];
-    userCardList.push(new User);
-
-    const userList = document.querySelector('.user-list');
-    
-    function renderUser(user) {
-        const userCard = document.createElement('div');
-        userCard.className = 'user-card';
-    
-        const userAvatar = document.createElement('div');
-        userAvatar.className = 'user-avatar';
-    
-        const avatarImg = document.createElement('img');
-        avatarImg.className = 'user-avatar-img';
-        avatarImg.src = user.avatar;
-        
-        const userDescriptionWrap = document.createElement('div');
-        userDescriptionWrap.className = 'user-description-wrap';
-    
-        const name = document.createElement('p');
-        name.className = 'user-name';
-    
-        const email = document.createElement('p');
-        email.className = 'user-email';
-    
-        const url = document.createElement('a');
-        url.className = 'user-url';
-        url.href = user.url;
-        url.target = '_blank';
-        
-        name.innerText = user.name;
-        email.innerText = user.email;
-        url.innerText = user.url;
-        userList.appendChild(userCard);
-        userCard.appendChild(userAvatar);
-        userAvatar.appendChild(avatarImg);
-        userCard.appendChild(userDescriptionWrap);
-        userDescriptionWrap.appendChild(name);
-        userDescriptionWrap.appendChild(email);
-        userDescriptionWrap.appendChild(url);
-    }
-
-    for (let i = 0; i < userCardList.length; i++) {
-        renderUser(userCardList[i]);
-    } 
-}
-
-
-
+    xhr.send();
+};
+getDataFromServer();
