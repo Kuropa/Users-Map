f6a80e107a527b448c6ece4fb3313948e1b724c3
Svetlana Vargo <drumminde@MacBook-Pro-Svetlana.local>
2019-08-12 18:50:32 +0300

    after rewew

--- javascript/user.js
+++ javascript/user.js
@@ -1,47 +1,47 @@
 function renderUser(user) {
-const userList = document.querySelector('.user-list');
+    const userList = document.querySelector('.user-list');
 
-const userCard = document.createElement('div');
-userCard.className = 'user-card';
+    const userCard = document.createElement('div');
+    userCard.className = 'user-card';
 
-const userAvatar = document.createElement('div');
-userAvatar.className = 'user-avatar';
+    const userAvatar = document.createElement('div');
+    userAvatar.className = 'user-avatar';
 
-const avatarImg = document.createElement('img');
-avatarImg.className = 'user-avatar-img';
-avatarImg.src = user.properties['avatar'];;
+    const avatarImg = document.createElement('img');
+    avatarImg.className = 'user-avatar-img';
+    avatarImg.src = user.properties['avatar'];;
 
-const userDescriptionWrap = document.createElement('div');
-userDescriptionWrap.className = 'user-description-wrap';
+    const userDescriptionWrap = document.createElement('div');
+    userDescriptionWrap.className = 'user-description-wrap';
 
-const name = document.createElement('p');
-name.className = 'user-name';
+    const name = document.createElement('p');
+    name.className = 'user-name';
 
-const email = document.createElement('p');
-email.className = 'user-email';
+    const email = document.createElement('p');
+    email.className = 'user-email';
 
-const url = document.createElement('a');
-url.className = 'user-url';
-url.href = user.properties['url'];
-url.target = '_blank';
+    const url = document.createElement('a');
+    url.className = 'user-url';
+    url.href = user.properties['url'];
+    url.target = '_blank';
 
-name.innerText = user.properties['userName'];
-email.innerText = user.properties['email'];;
-url.innerText = user.properties['url']
-userList.appendChild(userCard);
-userCard.appendChild(userAvatar);
-userAvatar.appendChild(avatarImg);
-userCard.appendChild(userDescriptionWrap);
-userDescriptionWrap.appendChild(name);
-userDescriptionWrap.appendChild(email);
-userDescriptionWrap.appendChild(url);
+    name.innerText = user.properties['userName'];
+    email.innerText = user.properties['email'];;
+    url.innerText = user.properties['url']
+    userList.appendChild(userCard);
+    userCard.appendChild(userAvatar);
+    userAvatar.appendChild(avatarImg);
+    userCard.appendChild(userDescriptionWrap);
+    userDescriptionWrap.appendChild(name);
+    userDescriptionWrap.appendChild(email);
+    userDescriptionWrap.appendChild(url);
 };
 
 function parseDataFromServerAndRenderUsers(data) {
     const users = JSON.parse(data).features;
     users.forEach(user => {
         renderUser(user);
-        addMarkersForMap(user);
+        addMarkerToMap(user);
     })
 };
 
